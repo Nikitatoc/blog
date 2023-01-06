ab9535c89f375081164786f369a6224c1f6b38a1
Nikitatoc <94145387+Nikitatoc@users.noreply.github.com>
2023-01-06 17:32:31 +0200

    Create register.php

---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <script src="js.js"></script>
+</head>
+<body>
+<form method="post" action="checkReg.php" class="RegisterForm">
+    <input type="email" id="email" name="email" placeholder="Введіть ваш email...">
+    <br>
+    <input type="text" id="name" name="name" placeholder="Введіть ваше ім'я...">
+    <br>
+    <input type="password" id="password" name="password" placeholder="Введіть ваш пароль...">
+    <br>
+    <input type="password" id="password" name="password2" placeholder="Підтвердити пароль">
+    <br>
+    <button type="submit" class="LoginButton">Зареєструватися</button>
+</form>
+
+</body>
+
