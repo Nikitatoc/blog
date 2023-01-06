5abb2a12993392197a458af8ac34b6048c74821c
Nikitatoc <94145387+Nikitatoc@users.noreply.github.com>
2023-01-06 17:19:51 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <script src="js.js"></script>
+</head>
+<body>
+
+<form method="post" action="checkLog.php" class="RegisterForm">
+    <input type="email" id="email" name="email" placeholder="Введіть ваш email...">
+    <br>
+    <input type="password" id="password" name="password" placeholder="Введіть ваш пароль...">
+    <br>
+    <button type="submit" class="LoginButton">Увійти</button>
+    <br>
+    <a href="blog.php" class="NoAccount" >Увійти без облікового запису</a>
+    <br>
+    <a href="register.php" class="NoAccount">В мене ще не має облікового запису</a>
+</form>
+</body>
